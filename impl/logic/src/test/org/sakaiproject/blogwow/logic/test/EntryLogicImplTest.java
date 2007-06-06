5d5dda4a9fcf5fffde56542296c9a3488fe59448
Aaron Zeckoski <azeckoski@vt.edu>
2007-06-06 11:38:54 +0000

    started writing tests

---
+++ impl/logic/src/test/org/sakaiproject/blogwow/logic/test/EntryLogicImplTest.java
@@ -0,0 +1,64 @@
+/******************************************************************************
+ * EntryLogicImplTest.java - created by Sakai App Builder -AZ
+ * 
+ * Copyright (c) 2006 Sakai Project/Sakai Foundation
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ *****************************************************************************/
+
+package org.sakaiproject.blogwow.logic.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing the Logic implementation methods
+ * @author Sakai App Builder -AZ
+ */
+public class EntryLogicImplTest extends TestCase {
+
+	/**
+	 * Test method for {@link org.sakaiproject.blogwow.logic.impl.EntryLogicImpl#canWriteEntry(java.lang.Long, java.lang.String)}.
+	 */
+	public void testCanWriteEntry() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.sakaiproject.blogwow.logic.impl.EntryLogicImpl#getAllVisibleEntries(java.lang.Long, java.lang.String, java.lang.String, boolean, int, int)}.
+	 */
+	public void testGetAllVisibleEntriesLongStringStringBooleanIntInt() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.sakaiproject.blogwow.logic.impl.EntryLogicImpl#getAllVisibleEntries(java.lang.Long[], java.lang.String, java.lang.String, boolean, int, int)}.
+	 */
+	public void testGetAllVisibleEntriesLongArrayStringStringBooleanIntInt() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.sakaiproject.blogwow.logic.impl.EntryLogicImpl#getEntryById(java.lang.Long)}.
+	 */
+	public void testGetEntryById() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.sakaiproject.blogwow.logic.impl.EntryLogicImpl#removeEntry(java.lang.Long)}.
+	 */
+	public void testRemoveEntry() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.sakaiproject.blogwow.logic.impl.EntryLogicImpl#saveEntry(org.sakaiproject.blogwow.model.BlogWowEntry)}.
+	 */
+	public void testSaveEntry() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+}
