677ec2d3892a9408d69a737e04a67002a4d965d9
Aaron Zeckoski <azeckoski@vt.edu>
2007-06-10 13:34:57 +0000

    Fixed blog locator to the standard way

--- tool/src/java/org/sakaiproject/blogwow/tool/otp/BlogLocator.java
+++ tool/src/java/org/sakaiproject/blogwow/tool/otp/BlogLocator.java
@@ -1,40 +1,51 @@
 package org.sakaiproject.blogwow.tool.otp;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import org.sakaiproject.blogwow.logic.BlogLogic;
+import org.sakaiproject.blogwow.logic.ExternalLogic;
 import org.sakaiproject.blogwow.model.BlogWowBlog;
 
 import uk.org.ponder.beanutil.BeanLocator;
 
 public class BlogLocator implements BeanLocator {
 
-	private BlogLogic blogLogic;
+    public static final String NEW_PREFIX = "new ";
+    public static String NEW_1 = NEW_PREFIX + "1";
 
-	private Map<String, BlogWowBlog> blogsmap = new HashMap<String, BlogWowBlog>();
+	private BlogLogic blogLogic;
+    private ExternalLogic externalLogic;
+
+	private Map<String, BlogWowBlog> delivered = new HashMap<String, BlogWowBlog>();
 
 	public Object locateBean(String name) {
-		BlogWowBlog togo = blogsmap.get(name);
-		if (togo == null) {
-			BlogWowBlog blog = blogLogic.getBlogById(new Long(name));
-			blog.setProfile("");
-			togo = blog;
-			blogsmap.put(name, togo);
-		}
-		return togo;
+        BlogWowBlog togo = delivered.get(name);
+        if (togo == null) {
+            if (name.startsWith(NEW_PREFIX)) {
+                // create the new blog object at the current location with the current user
+                togo = new BlogWowBlog(externalLogic.getCurrentLocationId(), externalLogic.getCurrentLocationId(), null);
+            } else {
+                togo = blogLogic.getBlogById(new Long(name));
+            }
+            delivered.put(name, togo);
+        }
+        return togo;
 	}
 
 	public String saveAll() {
-		Collection blogs = blogsmap.values();
-		for(Iterator i = blogs.iterator(); i.hasNext();) {
-			BlogWowBlog blog = (BlogWowBlog) i.next();
-			blogLogic.saveBlog(blog, blog.getLocation());
-		}
+        for (Iterator it = delivered.keySet().iterator(); it.hasNext();) {
+            String key = (String) it.next();
+            BlogWowBlog blog = delivered.get(key);
+            if (key.startsWith(NEW_PREFIX)) {
+                // could do stuff here
+            }
+            blogLogic.saveBlog(blog, blog.getLocation());
+        }
 		return "saved";
 	}
+
 
 	public void setBlogLogic(BlogLogic blogLogic) {
 		this.blogLogic = blogLogic;
