7ab6e5e2540e31de69bffe0d950581d9f47cec66
Aaron Zeckoski <azeckoski@vt.edu>
2007-06-10 15:03:59 +0000

    Fixed the home producer to not use wackiness

--- tool/src/java/org/sakaiproject/blogwow/tool/producers/HomeProducer.java
+++ tool/src/java/org/sakaiproject/blogwow/tool/producers/HomeProducer.java
@@ -1,7 +1,6 @@
 package org.sakaiproject.blogwow.tool.producers;
 
 import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -12,7 +11,6 @@ import org.sakaiproject.blogwow.model.BlogWowBlog;
 import org.sakaiproject.blogwow.model.BlogWowEntry;
 import org.sakaiproject.blogwow.tool.beans.MugshotGenerator;
 import org.sakaiproject.blogwow.tool.params.BlogParams;
-import org.sakaiproject.site.api.Site;
 
 import uk.org.ponder.rsf.components.UIBranchContainer;
 import uk.org.ponder.rsf.components.UIContainer;
@@ -34,43 +32,37 @@ public class HomeProducer implements ViewComponentProducer, DefaultView {
     }
 
     private NavBarRenderer navBarRenderer;
-
     private BlogLogic blogLogic;
-
     private EntryLogic entryLogic;
-
-    private Site site;
-
-    private String userid;
-
     private Locale locale;
-
     private MugshotGenerator mugshotGenerator;
-
     private ExternalLogic externalLogic;
 
 
-
     public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
+
+        String currentUserId = externalLogic.getCurrentUserId();
+        String locationId = externalLogic.getCurrentLocationId();
 
         // use a date which is related to the current users locale
         DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, locale);
 
         navBarRenderer.makeNavBar(tofill, "navIntraTool:", VIEWID);
 
-        BlogWowBlog myblog = blogLogic.getBlogByLocationAndUser(site.getReference(), userid );
+        BlogWowBlog myblog = blogLogic.getBlogByLocationAndUser(locationId, currentUserId );
         UIInternalLink.make(tofill, "my-blog-link", UIMessage.make("blogwow.homepage.userbloglink"), new BlogParams(BlogViewProducer.VIEWID, myblog.getId().toString()));
 
-        List<BlogWowEntry> myentries = entryLogic.getAllVisibleEntries(myblog.getId(), userid, null, true, 0, 1);
+        List<BlogWowEntry> myentries = entryLogic.getAllVisibleEntries(myblog.getId(), currentUserId, null, true, 0, 1);
         if (myentries.size() > 0) {
-            UIMessage.make(tofill, "last-blogged-date", "blogwow.homepage.userlastblogged", new Object[] { df.format( myentries.get(0).getDateModified() ) });
+            UIMessage.make(tofill, "last-blogged-date", "blogwow.homepage.userlastblogged", 
+                    new Object[] { df.format( myentries.get(0).getDateModified() ) });
         }
 
         UIMessage.make(tofill, "all-blogs-header", "blogwow.homepage.listofblogs");
 
         UIInternalLink.make(tofill, "all-blog-rss", UIMessage.make("blogwow.homepage.RSStext"), new SimpleViewParameters(BlogRSSProducer.VIEWID));
 
-        List<BlogWowBlog> blogs = blogLogic.getAllVisibleBlogs(site.getReference(), null, true, 0, 0);
+        List<BlogWowBlog> blogs = blogLogic.getAllVisibleBlogs(locationId, null, true, 0, 0);
         UIBranchContainer blogsTable = UIBranchContainer.make(tofill, "blog-list-table:");
 
         for (int i = 0; i < blogs.size(); i++) {
@@ -78,7 +70,7 @@ public class HomeProducer implements ViewComponentProducer, DefaultView {
             BlogWowBlog blog = blogs.get(i);
             UILink.make(row, "user-icon", mugshotGenerator.getMugshotUrl(blog.getOwnerId()));
             UIInternalLink.make(row, "blog-title-link", blog.getTitle(), new BlogParams(BlogViewProducer.VIEWID, blog.getId().toString()));
-            List<BlogWowEntry> entries = entryLogic.getAllVisibleEntries(blog.getId(), userid, null, true, 0, 1000);
+            List<BlogWowEntry> entries = entryLogic.getAllVisibleEntries(blog.getId(), currentUserId, null, true, 0, 1000);
             UIOutput.make(row, "number-of-entries", entries.size()+"");
             if (entries.size() > 0) {
                 UIOutput.make(row, "time-last-updated", df.format(entries.get(0).getDateModified()) );
@@ -92,6 +84,7 @@ public class HomeProducer implements ViewComponentProducer, DefaultView {
         }
 
     }
+
 
     public void setBlogLogic(BlogLogic blogLogic) {
         this.blogLogic = blogLogic;
@@ -111,14 +104,6 @@ public class HomeProducer implements ViewComponentProducer, DefaultView {
 
     public void setNavBarRenderer(NavBarRenderer navBarRenderer) {
         this.navBarRenderer = navBarRenderer;
-    }
-
-    public void setSite(Site site) {
-        this.site = site;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
     }
 
     public void setExternalLogic(ExternalLogic externalLogic) {
